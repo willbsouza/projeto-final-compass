1b3a0ad2cf09ea20f7c84edeeec7cd27347a5ade
Jefferson de Castro <Jeffersondecastro94@gmail.com>
2022-04-04 04:37:28 -0300

    delete endereco

--- src/main/java/com/compass/projetodoacao/controllers/EnderecoController.java
+++ src/main/java/com/compass/projetodoacao/controllers/EnderecoController.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -49,5 +50,12 @@ public class EnderecoController {
 	@Transactional
 	public ResponseEntity<Endereco> update(@PathVariable Integer id, @RequestBody @Valid EnderecoDTO enderecoDTO){
 		return ResponseEntity.ok(enderecoService.update(id, enderecoDTO));
+	}
+	
+	@DeleteMapping("/{id}")
+	@Transactional
+	public ResponseEntity<Void> deleteById(@PathVariable Integer id) {
+		enderecoService.deleteById(id);
+		return ResponseEntity.noContent().build();
 	}
 }
