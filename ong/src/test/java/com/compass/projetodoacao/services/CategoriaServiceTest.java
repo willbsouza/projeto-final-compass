908fa2fff414a0da9099c9b205953a335e4f08a0
Willams Souza <willamsbernardo@hotmail.com>
2022-04-12 10:48:12 -0300

    Testes de TelefoneService, EnderecoService e CategoriaService implementado

---
+++ ong/src/test/java/com/compass/projetodoacao/services/CategoriaServiceTest.java
@@ -0,0 +1,138 @@
+package com.compass.projetodoacao.services;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.compass.projetodoacao.entities.Categoria;
+import com.compass.projetodoacao.repositories.CategoriaRepository;
+import com.compass.projetodoacao.services.exception.ObjectNotFoundException;
+
+@SpringBootTest
+public class CategoriaServiceTest {
+
+	@InjectMocks
+	private CategoriaService categoriaService;
+
+	@Mock
+	private CategoriaRepository categoriaRepository;
+
+	private Categoria categoria;
+
+	private Optional<Categoria> optionalCategoria;
+
+	private final static Integer ID = 1;
+	private final static String NOME = "Vestuário";
+
+	@BeforeEach
+	void setUp() {
+		MockitoAnnotations.openMocks(this);
+		iniciarCategoria();
+	}
+
+	@Test
+	void deveriaRetornarUmaCategoriaAoBuscarPorId() {
+		when(categoriaRepository.findById(anyInt())).thenReturn(optionalCategoria);
+
+		Categoria response = categoriaService.findById(categoria.getId());
+
+		assertNotNull(response);
+		assertEquals(Categoria.class, response.getClass());
+		assertEquals(ID, response.getId());
+		assertEquals(NOME, response.getNome());
+	}
+
+	@Test
+	void deveriaRetornarUmaObjectNotFoundException() {
+		when(categoriaRepository.findById(anyInt()))
+				.thenThrow(new ObjectNotFoundException("ID: " + ID + " não encontrado."));
+
+		try {
+			categoriaService.findById(ID);
+		} catch (Exception e) {
+			assertEquals(ObjectNotFoundException.class, e.getClass());
+			assertEquals("ID: " + ID + " não encontrado.", e.getMessage());
+		}
+	}
+
+	@Test
+	void deveriaRetornarUmaListaDeCategorias() {
+		when(categoriaRepository.findAll()).thenReturn(List.of(categoria));
+
+		List<Categoria> response = categoriaService.findAll();
+
+		assertNotNull(response);
+		assertEquals(1, response.size());
+		assertEquals(Categoria.class, response.get(0).getClass());
+		assertEquals(ID, response.get(0).getId());
+		assertEquals(NOME, response.get(0).getNome());
+	}
+
+	@Test
+	void deveriaCriarUmaCategoria() {
+		when(categoriaRepository.save(any())).thenReturn(categoria);
+
+		Categoria response = categoriaService.save(categoria);
+
+		assertNotNull(response);
+		assertEquals(Categoria.class, response.getClass());
+		assertEquals(ID, response.getId());
+		assertEquals(NOME, response.getNome());
+	}
+	
+	@Test
+	void deveriaAtualizarUmaCategoriaComSucesso() {
+		when(categoriaRepository.findById(any())).thenReturn(optionalCategoria);
+		Categoria obj = optionalCategoria.get();	
+		Categoria response = categoriaService.update(anyInt(), obj);
+
+		assertNotNull(response);
+		assertEquals(Categoria.class, response.getClass());
+		assertEquals(ID, response.getId());
+		assertEquals(NOME, response.getNome());
+	}
+
+	@Test
+	void deveriaDeletarUmaCategoriaComSucesso() {
+		when(categoriaRepository.findById(anyInt())).thenReturn(optionalCategoria);
+		Mockito.doNothing().when(categoriaRepository).deleteById(anyInt());
+		categoriaService.deleteById(ID);
+
+		verify(categoriaRepository, times(1)).deleteById(anyInt());
+	}
+
+	@Test
+	void deveriaRetornarUmaObjectNotFoundExceptionAoTentarDeletarUmaCategoria() {
+		when(categoriaRepository.findById(anyInt()))
+				.thenThrow(new ObjectNotFoundException("ID: " + ID + " não encontrado."));
+		try {
+			categoriaService.deleteById(ID);
+		} catch (Exception e) {
+			assertEquals(ObjectNotFoundException.class, e.getClass());
+			assertEquals("ID: " + ID + " não encontrado.", e.getMessage());
+		}
+	}
+
+	private void iniciarCategoria() {
+		categoria = new Categoria();
+		categoria.setId(ID);
+		categoria.setNome(NOME);
+
+		optionalCategoria = Optional.of(categoria);
+	}
+}
---
+++ ong/src/test/java/com/compass/projetodoacao/services/TelefoneServiceTest.java
@@ -0,0 +1,144 @@
+package com.compass.projetodoacao.services;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.compass.projetodoacao.dto.TelefoneFormDTO;
+import com.compass.projetodoacao.entities.Telefone;
+import com.compass.projetodoacao.repositories.TelefoneRepository;
+import com.compass.projetodoacao.services.exception.ObjectNotFoundException;
+
+@SpringBootTest
+public class TelefoneServiceTest {
+
+	@InjectMocks
+	private TelefoneService telefoneService;
+
+	@Mock
+	private TelefoneRepository telefoneRepository;
+
+	private Telefone telefone;
+	
+	private TelefoneFormDTO telefoneDTO;
+
+	private Optional<Telefone> optionalTelefone;
+
+	private final static Integer ID = 1;
+	private final static String NUMERO = "81987654321";
+	
+	@BeforeEach
+	void setUp() {
+		MockitoAnnotations.openMocks(this);
+		iniciarTelefone();
+	}
+
+	@Test
+	void deveriaRetornarUmTelefoneAoBuscarPorId() {
+		when(telefoneRepository.findById(anyInt())).thenReturn(optionalTelefone);
+
+		Telefone response = telefoneService.findById(telefone.getId());
+
+		assertNotNull(response);
+		assertEquals(Telefone.class, response.getClass());
+		assertEquals(ID, response.getId());
+		assertEquals(NUMERO, response.getNumero());
+	}
+
+	@Test
+	void deveriaRetornarUmaObjectNotFoundException() {
+		when(telefoneRepository.findById(anyInt()))
+				.thenThrow(new ObjectNotFoundException("ID: " + ID + " não encontrado."));
+
+		try {
+			telefoneService.findById(ID);
+		} catch (Exception e) {
+			assertEquals(ObjectNotFoundException.class, e.getClass());
+			assertEquals("ID: " + ID + " não encontrado.", e.getMessage());
+		}
+	}
+
+	@Test
+	void deveriaRetornarUmaListaDeTelefones() {
+		when(telefoneRepository.findAll()).thenReturn(List.of(telefone));
+
+		List<Telefone> response = telefoneService.findAll();
+
+		assertNotNull(response);
+		assertEquals(1, response.size());
+		assertEquals(Telefone.class, response.get(0).getClass());
+		assertEquals(ID, response.get(0).getId());
+		assertEquals(NUMERO, response.get(0).getNumero());
+	}
+
+	@Test
+	void deveriaCriarUmTelefone() {
+		when(telefoneRepository.save(any())).thenReturn(telefone);
+
+		Telefone response = telefoneService.saveTelefone(telefoneDTO);
+
+		assertNotNull(response);
+		assertEquals(Telefone.class, response.getClass());
+		assertEquals(ID, response.getId());
+		assertEquals(NUMERO, response.getNumero());
+	}
+	
+	@Test
+	void deveriaAtualizarUmTelefoneComSucesso() {
+		when(telefoneRepository.findById(any())).thenReturn(optionalTelefone);
+		Telefone response = telefoneService.update(anyInt(), telefoneDTO);
+
+		assertNotNull(response);
+		assertEquals(Telefone.class, response.getClass());
+		assertEquals(ID, response.getId());
+		assertEquals(NUMERO, response.getNumero());
+	}
+
+	@Test
+	void deveriaDeletarUmTelefoneComSucesso() {
+		when(telefoneRepository.findById(anyInt())).thenReturn(optionalTelefone);
+		Mockito.doNothing().when(telefoneRepository).deleteById(anyInt());
+		telefoneService.deleteById(ID);
+
+		verify(telefoneRepository, times(1)).deleteById(anyInt());
+	}
+
+	@Test
+	void deveriaRetornarUmaObjectNotFoundExceptionAoTentarDeletarUmTelefone() {
+		when(telefoneRepository.findById(anyInt()))
+				.thenThrow(new ObjectNotFoundException("ID: " + ID + " não encontrado."));
+		try {
+			telefoneService.deleteById(ID);
+		} catch (Exception e) {
+			assertEquals(ObjectNotFoundException.class, e.getClass());
+			assertEquals("ID: " + ID + " não encontrado.", e.getMessage());
+		}
+	}
+
+	private void iniciarTelefone() {
+		telefone = new Telefone();
+		telefone.setId(ID);
+		telefone.setNumero(NUMERO);
+
+		optionalTelefone = Optional.of(telefone);
+		
+		telefoneDTO = new TelefoneFormDTO();
+		telefoneDTO.setId(ID);
+		telefoneDTO.setNumero(NUMERO);
+	}
+}
